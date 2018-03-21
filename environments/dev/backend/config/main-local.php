545353e551a73555c46bd7048e8f11da32450cf8
unknown <termit_90@mail.ru>
2018-03-22 03:18:18 +0500

    added files

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -5,13 +5,13 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs' => ['185.74.5.2'],
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs' => ['185.74.5.2'],
+        'allowedIPs' => ['*'],
     ];
 }
 
